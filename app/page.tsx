610a62307b94c6b6197fb92d91589221bcf88a6a
Sagar Shekhar <70952463+Sagar7777521@users.noreply.github.com>
2023-12-18 00:56:43 +0530

    Clerk is implemented but not able to run

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Nextjs Here we come!!!!</h1>
-      <h1 className="h2-bold">Nextjs Here we come!!!!</h1>
-      <h1 className="h3-bold">Nextjs Here we come!!!!</h1>
-    </div>
-  );
-}
