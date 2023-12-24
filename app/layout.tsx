56af28bda7e467c44af83b09c0d8fef0183bb9f4
Sagar Shekhar <70952463+Sagar7777521@users.noreply.github.com>
2023-12-25 01:42:33 +0530

    Assets Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "DevOverFlow",
+  description:
+    "DevOverFlow is a question-and-answer platform for professional and enthusiast programmers",
+
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
@@ -10,6 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <h1 className="h1-bold">This is Text!dddd!!!sss!!</h1>
         <body>{children}</body>
       </html>
     </ClerkProvider>
